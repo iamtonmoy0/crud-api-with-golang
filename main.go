b3f52d34da7753eb2faaea9796098b8a6a8f0b5f
iamtonmoy0 <edith@localhost.localdomain>
2022-12-15 22:46:26 +0600

    create function created

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,6 +19,32 @@ type Director struct {
 }
 
 var movies []Movie
+func getMovies(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json NewEncoder(w).Encode(movies)
+}
+func deleteMovie(w http.ResponseWriter, r *http.Request ) {
+	w.Header().set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index,item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index],movies[index+1]... )
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+	
+}
+func getMovie(w http.ResponseWriter,r *http.Request){
+w.Header().Set("Content-type","application/json")
+params := mux.Vars(r)
+fot _, item := range movies{
+	if item.ID == params["id"]{
+		json.NewEncoder(w).Encode(item)
+		return
+	}
+}
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -31,5 +58,5 @@ func main() {
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("starting server ar port 8000\n")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
