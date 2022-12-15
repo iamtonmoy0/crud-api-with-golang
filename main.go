b329e4ea95c9be13ce41cbb915d767dd5e9663fc
iamtonmoy0 <edith@localhost.localdomain>
2022-12-15 06:09:20 +0600

    movie added

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -14,9 +20,16 @@ type Director struct {
 var movies []Movie
 
 func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "45454", Title: "movie", Director: &Director{Firstname: "john", Lastname: "doe"}})
+	movies = append(movies, Movie{ID: "1", Isbn: "45454", Title: "movie", Director: &Director{Firstname: "john", Lastname: "doe"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting server ar port 8000\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
