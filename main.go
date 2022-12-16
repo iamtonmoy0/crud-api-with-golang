989229979f0fe26fbd3d7f179168e0415ef906e6
iamtonmoy0 <edith@localhost.localdomain>
2022-12-17 00:56:06 +0600

    update func added

--- main.go
+++ main.go
@@ -59,7 +59,21 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
 func main() {
 	r := mux.NewRouter()
 
